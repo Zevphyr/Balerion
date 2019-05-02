ca3c505911e4c96e4798aee28e82e3ce126e816b
Kyle Santiago <44658723+Zevphyr@users.noreply.github.com>
2019-05-02 14:12:10 -0500

    Update balerion_main.py
    fixed intial_extensions

--- Balerion/balerion_main.py
+++ Balerion/balerion_main.py
@@ -29,10 +29,10 @@ bot = commands.Bot(command_prefix=get_prefix, description=description)
 
 # Below cogs represents our folder our cogs are in. Following is the file name. So 'meme.py' in cogs, would be cogs.meme
 # Think of it like a dot path import
-initial_extensions = ['cog.owner',
-                      'cog.role',
-                      'cog.admin',
-                      'cog.members']
+initial_extensions = ['cogs.owner',
+                      'cogs.role',
+                      'cogs.admin',
+                      'cogs.members']
 
 # login
 @bot.event
