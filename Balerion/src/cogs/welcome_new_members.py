6f0dedc7fa682d60f2e99a8c90f441aa31134c8c
lringham <leeringham@gmail.com>
2019-10-05 09:55:11 -0600

    V2.4.0(#30)
    New users are welcomed in the introduce-yourself channel and they are prompted to join the assign-yourself-ranks channel

---
+++ Balerion/src/cogs/welcome_new_members.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+
+
+class WelcomeNewMembersCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member: discord.Member):
+        """ Welcome new user in the introduce-yourself channel and prompt to join the assign-yourself-ranks channel """
+
+        introduce_yourself_channel = discord.utils.get(self.bot.get_all_channels(), name='introduce-yourself')
+        assign_ranks_channel = discord.utils.get(self.bot.get_all_channels(), name='assign-yourself-ranks')
+        welcome_string = f'Welcome {member.mention}! Please provide a brief introduction here and visit \
+        <#{assign_ranks_channel.id}> to specify the programming language(s) you are interested in.'
+
+        await introduce_yourself_channel.send(welcome_string)
+
+def setup(bot):
+    bot.add_cog(WelcomeNewMembersCog(bot))
